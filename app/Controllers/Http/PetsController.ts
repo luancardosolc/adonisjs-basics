f9211058091458ef27ca0fa9cfa13d771fe0144f
Luan Cardoso <luanleitecardoso@gmail.com>
2022-01-30 16:50:11 -0300

    Adding validation

--- app/Controllers/Http/PetsController.ts
+++ app/Controllers/Http/PetsController.ts
@@ -1,4 +1,5 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext';
+import { schema } from '@ioc:Adonis/Core/Validator';
 import Pet from 'App/Models/Pet';
 
 export default class PetsController {
@@ -7,8 +8,12 @@ export default class PetsController {
   }
 
   public async store({ request, response }: HttpContextContract) {
-    const body = request.body();
-    const pet = await Pet.create(body);
+    const newPetSchema = schema.create({
+      name: schema.string({ trim: true }),
+    });
+    const payload = await request.validate({ schema: newPetSchema });
+
+    const pet = await Pet.create(payload);
     response.status(201);
 
     return { pet };
